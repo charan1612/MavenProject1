d11c5c0c47f41ecf302c7fd62afa4e202c008649
charan1612 <145385613+charan1612@users.noreply.github.com>
2023-11-23 21:19:17 +0530

    Add files via upload

---
+++ src/main/java/SE_Group/SpideyMavenProject1/App.java
@@ -0,0 +1,13 @@
+package SE_Group.SpideyMavenProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
